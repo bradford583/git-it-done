83d76349aa6459bf6e1d983d704d54f37b0514f7
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-08-27 19:12:58 -0600

    finished list issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -40,6 +41,7 @@ var formSubmitHandler = function (event) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
@@ -56,8 +58,9 @@ var displayRepos = function (repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         //create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // create a span element to hold repo name
         var titleEl = document.createElement("span");
